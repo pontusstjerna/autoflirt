00424a00078075a7634a7690567f65e1bc0208ac
Pontus Stjernström <pontus@wmyl.se>
2019-08-23 16:01:25 +0200

    Made a super simple rest API for requesting raggningsrepliker.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
-import _generateEN from './model_en';
-import _generateSE from './model_se';
+import express from 'express';
+import generateEN from './model_en';
+import generateSE from './model_se';
 import * as allWordsSE from './words/se';
-
-export const generateEN = () => {
-    return _generateEN();
-}
-
-export const generateSE = (isSerious, isMean) => {
-    return _generateSE(isSerious, isMean);
-}
 
 export const wordsSE = () => {
     return Object.keys(allWordsSE)
         .map(wordType => allWordsSE[wordType])
         .map(words => words.serious.length + words.mean.length + words.weird.length)
         .reduce((prev, curr) => prev * curr);
-}
+};
 
-/*setInterval(() => { 
-    console.log(generateSE(false, true));   
-}, 20);*/
+const app = express();
+const port = 3001;
+let requestsSE = 0;
+let requestsEN = 0;
 
-// generate().then(line => {    
-//     console.log('*********PICK UP LINE************\n');
-//     console.log(line);
-//     console.log('\n*********************************');
-// });
+app.get('/se/', (req, res) => {
+    requestsSE++;
+    let serious = req.query.serious !== "false";
+    let mean = req.query.mean === "true";
+    res.send(generateSE(serious, mean));
+});
+
+app.get('/en/', (req, res) => {
+   requestsEN++;
+   res.send("Not supported yet.");
+});
+
+app.get('/meta/', (req, res) => {
+   res.send(JSON.stringify({
+       requestsEN,
+       requestsSE,
+       requests: requestsEN + requestsSE
+   }));
+});
+
+app.listen(port, () => console.log(`Autoflirt API listening at ${port}`));
