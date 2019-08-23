f4a085302623209509870eb41c9b76794933fa29
Pontus Stjernström <pontus@wmyl.se>
2019-08-23 16:08:19 +0200

    removed dist folder

--- dist/util/stringUtils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-var upperCaseFirst = exports.upperCaseFirst = function upperCaseFirst(string) {
-    return string.charAt(0).toUpperCase() + string.substring(1);
-};
