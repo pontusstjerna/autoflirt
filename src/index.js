f5c4713e75a28c2d4d89b0cf57b5a7621790cc8e
Pontus <pontusstjernstrom@gmail.com>
2019-09-15 17:18:09 +0200

    fix count again

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.get('/se/', (req, res) => {
     generateSE(serious, mean).then(line => res.send(line));
 });
 
-app.get('/se/count', (req, res) => res.status(200).send(wordsSE()));
+app.get('/se/count', (req, res) => res.status(200).send("" + wordsSE()));
 
 app.get('/en/', (req, res) => {
    requestsEN++;
